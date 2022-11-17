f53ebcf0f95b94de6c83d2518a8788fdb1dbef83
Faris Putra <farisp1212@gmail.com>
2022-11-17 12:03:29 +0700

    Small changes

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 import pymongo
 app = Flask(__name__)
-client = pymongo.MongoClient("localhost",27017)
+client = pymongo.MongoClient("localhost", 27017)
 
 pond = {
     'name': "alpha", 
@@ -10,44 +10,44 @@ pond = {
     'shape': "bundar"
     }
 activation = {
-    'name': "alpha",
-    'ikan': "lele",
-    'jumlah': "200",
-    'berat': "50",
-    'ketinggian': "1",
-    'isactive': False,
-    'tanggal': "19-01-2022"
+    'pond_name': "alpha",
+    'fish_species': "lele",
+    'fish_count': "200",
+    'total_weight': "50",
+    'water_depth': "1",
+    'is_active': False,
+    'activation_date': "19-01-2022"
     }
 db = client.fishdb
 db.pond.insert_one(pond)
 db.activation.insert_one(activation)
 
-@app.route("/api/v1/registrasi", methods=["POST"])
-def register():
+@app.route("/api/v1/ponds/registration", methods=["POST"])
+def register_pond():
     getdata = request.form()
     result = db.pond.insert_one(jsonify(getdata))
     if result:
         return True
     return False
 
-@app.route("/api/v1/aktifasi/<pondname>", methods=["POST"])
+@app.route("/api/v1/ponds/activation/<pondname>", methods=["POST"])
 def activation(pondname):
-    data = db.activation.find_one({"name":pondname})
+    data = db.activation.find_one({"name": pondname})
     getdata = request.form()
-    data["name"] = getdata["name"]
-    data["ikan"] = getdata["ikan"]
-    data["jumlah"] = getdata["jumlah"]
-    data["ketinggian"] = getdata["ketinggian"]
-    data["isactive"] = getdata["isactive"]
-    data["tanggal"] = getdata["tanggal"]
+    data["pond_name"] = getdata["pond_name"]
+    data["fish_species"] = getdata["fish_species"]
+    data["fish_count"] = getdata["fish_count"]
+    data["water_depth"] = getdata["water_depth"]
+    data["is_active"] = getdata["is_active"]
+    data["activation_date"] = getdata["activation_date"]
     db.activation.update_one({"name": pondname}, {"$set": data})
 
-@app.route("/api/v1/pondinfo", methods=["GET"])
+@app.route("/api/v1/ponds/info", methods=["GET"])
 def pondinfo():
     data = db.pond.find()
     return data
 
-@app.route("/api/v1/pondinfo/<pondname>", methods=["GET"])
+@app.route("/api/v1/ponds/info/<pondname>", methods=["GET"])
 def pondstatus(pondname):
     data = db.pond.find_one({"name": pondname}, {})
     return data
